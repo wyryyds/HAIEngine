8b3f3c853b6fad94c620bb71f8f79140333673f0
Sean <2857970654@qq.com>
2023-04-11 19:01:58 +0800

    add xmake

---
+++ build/.gens/rules/plugin.vsxmake.autoupdate.d
@@ -0,0 +1,24 @@
+{
+    files = {
+        [[C:\C++Projects\GameEngineLearning\xmake.lua]]
+    },
+    values = {
+        [[HAIEngine\src\HAIEngine.h]],
+        [[HAIEngine\src\HAIEngine\Application.cpp]],
+        [[HAIEngine\src\HAIEngine\Application.h]],
+        [[HAIEngine\src\HAIEngine\Core.h]],
+        [[HAIEngine\src\HAIEngine\EntryPoint.h]],
+        [[HAIEngine\src\HAIEngine\Events\ApplicationEvent.h]],
+        [[HAIEngine\src\HAIEngine\Events\Event.h]],
+        [[HAIEngine\src\HAIEngine\Events\KeyEvent.h]],
+        [[HAIEngine\src\HAIEngine\Events\MouseEvent.h]],
+        [[HAIEngine\src\HAIEngine\Log.cpp]],
+        [[HAIEngine\src\HAIEngine\Log.h]],
+        [[HAIEngine\src\HAIEngine\Window.h]],
+        [[HAIEngine\src\Platform\WindowsWindow.cpp]],
+        [[HAIEngine\src\Platform\WindowsWindow.h]],
+        [[HAIEngine\src\hepch.cpp]],
+        [[HAIEngine\src\hepch.h]],
+        [[Sandbox\src\SandboxApp.cpp]]
+    }
+}
---
+++ xmake.lua
@@ -0,0 +1,27 @@
+add_rules("mode.debug", "mode.release","plugin.vsxmake.autoupdate")
+
+add_requires("spdlog","glfw")
+
+ if is_plat("windows") then
+    add_defines("HE_PLATFORM_WINDOWS")
+end
+
+if is_mode("debug") then
+    add_defines("HE_ENABLE_ASSERTS")
+end
+
+target("Sandbox")
+    set_kind("binary")
+    add_files("Sandbox/src/**.cpp")
+    add_includedirs("HAIEngine/src")
+    add_packages("spdlog")
+    add_deps("HAIEngine")
+    add_headerfiles("Sandbox/src/**.h")
+
+target("HAIEngine")
+    set_kind("shared")
+    add_includedirs("HAIEngine/src")
+    add_files("HAIEngine/src/**.cpp")
+    add_packages("spdlog","glfw")
+    add_defines("HE_BUILD_DLL")
+    add_headerfiles("HAIEngine/src/**.h")
